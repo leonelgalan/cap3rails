ea7cd966433a7c7b18408143ca6090ef4bbb8eb8
Leonel Galan <leonelgalan@gmail.com>
2014-01-21 11:33:48 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cap3rails::Application.config.secret_key_base = 'e799bb34414e7ee925e7278e0a74a9005778a06f48f4a2bc3d7822f3a1d31868c83c28230f032c57689b177d6cd74010705447778c18bb6212784c677e85d36e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cap3rails::Application.config.session_store :cookie_store, key: '_cap3rails_session'
